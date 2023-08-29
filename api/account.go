352038d15289556488159a1a1611aeb18c273dc6
Egor Aksenov <weqqu2014@yandex.ru>
2023-08-29 15:09:48 +0300

    fix delete method

--- api/account.go
+++ api/account.go
@@ -94,11 +94,12 @@ type deleteAccountRequest struct {
 func (server *Server) deleteAccount(ctx *gin.Context) {
 	var req deleteAccountRequest
 	if err := ctx.ShouldBindUri(&req); err != nil {
-		ctx.JSON(http.StatusNoContent, errorResponse(err))
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
 
-	if err := server.store.DeleteAccount(ctx, req.ID); err != nil {
+	account, err := server.store.GetAccount(ctx, req.ID)
+	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			ctx.JSON(http.StatusNotFound, errorResponse(err))
 			return
@@ -107,4 +108,12 @@ func (server *Server) deleteAccount(ctx *gin.Context) {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
+
+	err = server.store.DeleteAccount(ctx, req.ID)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
 }
