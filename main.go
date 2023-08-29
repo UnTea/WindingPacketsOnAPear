36566acecf8526a4a9f66c3c5a45ae034956010e
Egor Aksenov <weqqu2014@yandex.ru>
2023-08-29 15:42:16 +0300

    replace hard-coding env variable

--- main.go
+++ main.go
@@ -3,20 +3,16 @@ package main
 import (
 	"database/sql"
 	"log"
+	"simplebank/util"
 
 	_ "github.com/lib/pq"
 	"simplebank/api"
 	db "simplebank/db/sqlc"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:root@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to database:", err)
 	}
@@ -24,7 +20,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server: ", err)
 	}
