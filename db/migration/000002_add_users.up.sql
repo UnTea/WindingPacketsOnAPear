0607e158a7f62a990ef8b7fb54b3272831adb615
UnTea <weqqu2014@yandex.ru>
2023-09-01 10:36:46 +0300

    fix typos

--- db/migration/000002_add_users.up.sql
+++ db/migration/000002_add_users.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "users" (
     "username"         varchar PRIMARY KEY,
     "hashed_password"  varchar        NOT NULL,
-    "fullname"         varchar        NOT NULL,
+    "full_name"        varchar        NOT NULL,
     "email"            varchar UNIQUE NOT NULL,
     "password_changed" timestamptz    NOT NULL DEFAULT '0001-01-01 00:00Z',
     "created_at"       timestamptz    NOT NULL DEFAULT (now())
@@ -11,4 +11,5 @@ ALTER TABLE "accounts"
     ADD FOREIGN KEY ("owner") REFERENCES "users" ("username");
 
 -- CREATE UNIQUE INDEX ON "accounts" ("owner", "currency");
-ALTER TABLE "accounts" ADD CONSTRAINT "owner_currency_key" UNIQUE ("owner", "currency");
+ALTER TABLE "accounts"
+    ADD CONSTRAINT "owner_currency_key" UNIQUE ("owner", "currency");
